aa89f4ac35ea41ab278e41e478e2d850c3e51f5a
Kimit <kimheetae90@gmail.com>
2022-05-06 19:10:02 +0900

    [init]Project Setting

---
+++ xlmlight/xlmlight/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xlmlight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
